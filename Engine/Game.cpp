47f1fc109f9298f585414ec445a87f465d663ffa
Alias-nsk <alias1322@gmail.com>
2022-06-01 20:11:12 +0700

    Tutorial 2 - Variables & constants

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -42,16 +42,36 @@ void Game::UpdateModel()
 
 void Game::ComposeFrame()
 {
-	gfx.PutPixel(720, 560, 255, 255, 255);
-	gfx.PutPixel(721, 560, 255, 255, 255);
-	gfx.PutPixel(722, 560, 255, 255, 255);
-	gfx.PutPixel(728, 560, 255, 255, 255);
-	gfx.PutPixel(729, 560, 255, 255, 255);
-	gfx.PutPixel(730, 560, 255, 255, 255);
-	gfx.PutPixel(725, 555, 255, 255, 255);
-	gfx.PutPixel(725, 556, 255, 255, 255);
-	gfx.PutPixel(725, 557, 255, 255, 255);
-	gfx.PutPixel(725, 563, 255, 255, 255);
-	gfx.PutPixel(725, 564, 255, 255, 255);
-	gfx.PutPixel(725, 565, 255, 255, 255);
+	const int x = Graphics::ScreenWidth / 2;
+    const int y = Graphics::ScreenHeight / 2; //Center of the crosshair
+
+
+	gfx.PutPixel(x - 5, y,     255, 255, 255);
+	gfx.PutPixel(x - 4, y,     255, 255, 255);
+	gfx.PutPixel(x - 3, y,     255, 255, 255);
+	gfx.PutPixel(x + 3, y,     255, 255, 255);
+	gfx.PutPixel(x + 4, y,     255, 255, 255);
+	gfx.PutPixel(x + 5, y,     255, 255, 255);
+	gfx.PutPixel(x,     y - 5, 255, 255, 255);
+	gfx.PutPixel(x,     y - 4, 255, 255, 255);
+	gfx.PutPixel(x,     y - 3, 255, 255, 255);
+	gfx.PutPixel(x,     y + 3, 255, 255, 255);
+	gfx.PutPixel(x,     y + 4, 255, 255, 255);
+	gfx.PutPixel(x,     y + 5, 255, 255, 255);
+	
+	/*
+	gfx.PutPixel(695, 500, 255, 255, 255);
+	gfx.PutPixel(696, 500, 255, 255, 255);
+	gfx.PutPixel(697, 500, 255, 255, 255);
+	gfx.PutPixel(703, 500, 255, 255, 255);
+	gfx.PutPixel(704, 500, 255, 255, 255);
+	gfx.PutPixel(705, 500, 255, 255, 255);
+	
+	gfx.PutPixel(700, 495, 255, 255, 255);
+	gfx.PutPixel(700, 496, 255, 255, 255);
+	gfx.PutPixel(700, 497, 255, 255, 255);
+	gfx.PutPixel(700, 503, 255, 255, 255);
+	gfx.PutPixel(700, 504, 255, 255, 255);
+	gfx.PutPixel(700, 505, 255, 255, 255);
+	*/
 }
